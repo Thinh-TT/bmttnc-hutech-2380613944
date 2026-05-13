8478a0ddd8808f1c1c4af4c03af8a8975764ee92
Thinh-TT <npk77734351903@gmail.com>
2026-05-13 11:27:53 +0700

    add: lab01

---
+++ lab_01/exe02_07.py
@@ -0,0 +1,12 @@
+print("Nhap cac dong van ban (Nhap 'done' de ket thuc): ")
+lines = []
+while True:
+    line = input()
+    if line.lower() =='done':
+        break
+    lines.append(line)
+
+print("\n Cac dong da nhap sau khi chuyen in hoa: ")
+for line in lines:
+    print(line.upper())
+    
